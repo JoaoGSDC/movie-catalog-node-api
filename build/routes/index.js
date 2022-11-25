2678d127c99ff11c059464e3086ff0d6f06d18d7
JoaoGSDC <joaogsdc@gmail.com>
2022-11-25 18:08:12 -0300

    heroku init

--- build/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const movies_routes_1 = require("./movies.routes");
-const routes = (0, express_1.Router)();
-exports.routes = routes;
-routes.use('/movies', movies_routes_1.moviesRoutes);
